474ce3da12719c99b9bbb019edb49c0095786a72
Chris Hut <tophernuts@gmail.com>
2020-09-24 13:25:52 -0600

    Just use the latest upstreamTransformer

--- index.js
+++ index.js
@@ -8,25 +8,26 @@ var upstreamTransformer = null;
 var reactNativeVersionString = require("react-native/package.json").version;
 var reactNativeMinorVersion = semver(reactNativeVersionString).minor;
 
-if (reactNativeMinorVersion >= 59) {
-  upstreamTransformer = require("metro-react-native-babel-transformer");
-} else if (reactNativeMinorVersion >= 56) {
-  upstreamTransformer = require("metro/src/reactNativeTransformer");
-} else if (reactNativeMinorVersion >= 52) {
-  upstreamTransformer = require("metro/src/transformer");
-} else if (reactNativeMinorVersion >= 47) {
-  upstreamTransformer = require("metro-bundler/src/transformer");
-} else if (reactNativeMinorVersion === 46) {
-  upstreamTransformer = require("metro-bundler/build/transformer");
-} else {
-  // handle RN <= 0.45
-  var oldUpstreamTransformer = require("react-native/packager/transformer");
-  upstreamTransformer = {
-    transform({ src, filename, options }) {
-      return oldUpstreamTransformer.transform(src, filename, options);
-    }
-  };
-}
+upstreamTransformer = require("metro-react-native-babel-transformer");
+// if (reactNativeMinorVersion >= 59) {
+//   upstreamTransformer = require("metro-react-native-babel-transformer");
+// } else if (reactNativeMinorVersion >= 56) {
+//   upstreamTransformer = require("metro/src/reactNativeTransformer");
+// } else if (reactNativeMinorVersion >= 52) {
+//   upstreamTransformer = require("metro/src/transformer");
+// } else if (reactNativeMinorVersion >= 47) {
+//   upstreamTransformer = require("metro-bundler/src/transformer");
+// } else if (reactNativeMinorVersion === 46) {
+//   upstreamTransformer = require("metro-bundler/build/transformer");
+// } else {
+//   // handle RN <= 0.45
+//   var oldUpstreamTransformer = require("react-native/packager/transformer");
+//   upstreamTransformer = {
+//     transform({ src, filename, options }) {
+//       return oldUpstreamTransformer.transform(src, filename, options);
+//     }
+//   };
+// }
 
 module.exports.transform = function(src, filename, options) {
   if (typeof src === "object") {
